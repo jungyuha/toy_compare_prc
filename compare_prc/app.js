fc93fea2172e4ed99358352fb18c472e654f52ce
jungyuha <yooha9621@gmail.com>
2022-06-06 15:29:06 +0900

    *) 부트스트랩 디자인적용

--- compare_prc/app.js
+++ compare_prc/app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var unitTestRouter = require('./routes/unitTest');
 
 var app = express();
 
@@ -19,8 +18,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/statics/css',express.static(path.join(__dirname, 'public/javascripts')));
+app.use('/statics/images',express.static(path.join(__dirname, 'public/images')));
+
+app.use('/unitTest', unitTestRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
